8c0283bae0ad5f90813305fcd447e8ea9e034b99
Victor Maldonado <49620183+vmh82@users.noreply.github.com>
2024-07-04 12:18:03 -0500

    Version inicial ws contapp

---
+++ src/main/java/com/contapp/cargaParametros/CargaParametrosApplication.java
@@ -0,0 +1,13 @@
+package com.contapp.cargaParametros;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CargaParametrosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CargaParametrosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/contapp/cargaParametros/CargaParametrosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.contapp.cargaParametros;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CargaParametrosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
